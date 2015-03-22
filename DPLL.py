17e2477da6e4ffd39214be69cba8052e4ec5cc18
Pradtha <pradeep1005@hotmail.com>
2015-03-21 17:56:33 -0700

    implemented find pure symbols

--- DPLL.py
+++ DPLL.py
@@ -4,10 +4,6 @@ inputFile = open(sys.argv[2])
 outputFile = open("CNF_sastisfiability.txt", "w")
 
 operators = ["not", "and", "or"]
-
-class index(object):
-	i=0
-
 	
 def booleanStringClause(clause):
 	if(not(isinstance(clause,list))):
@@ -45,31 +41,124 @@ def getClauses(model):
 		clauses.append(model)
 		
 	return clauses
+	
+def checkComplementarity(element):
+	complemented = False
+	variable = ""
+	if(not(isinstance(element,list))):
+		complemented = False
+		variable = element
+	
+	else:
+		if(element[0] == 'not'):
+			complemented = True
+			variable = element[1]
+
+	#print "com,var", complemented,variable
+	return complemented,variable
+	
+def updateValueList(complemented, value, index):
+	#print "value[index]: ",value[index]
+	if(complemented == False and value[index] == 0):
+		return 1
+	if(complemented == True and value[index] == 0):
+		return 2
+	if((complemented == False and value[index] == 2) or (complemented == True and value[index] == 1)):
+		return 3
+	return value[index]
+	
 		
-def findPureSymbols(symbols,clauses,model):
-	symbols = getSymbols(model)
-	clauses = getClauses(model)
+def findPureSymbols(clauses,symbols):
 	pureList = []
 	value = []
+	
 	for symbol in symbols:
-		pureList.append(symbol)
 		value.append(0)
-		
-		
-def DPLL(clauses,symbols,model):
-	result = False
-	#print symbols, clauses
+	
+	#print "clauses", clauses
 	for clause in clauses:
-		print clause,"   ", booleanStringClause(clause)
+		#print "inival", value
+		if(not(isinstance(clause,list))):
+			#print "literal"
+			index = symbols.index(clause)
+			#print index
+			value[index] = updateValueList(False,value,index)
 		
-	print "end of sentence"
+		else:
+			if(clause[0] == 'not'):
+				#print "com"
+				index = symbols.index(clause[1])
+				#print index
+				value[index] = updateValueList(True,value,index)
+			
+			else:
+				for element in clause[1:len(clause)]:
+					#print "or"
+					complemented, variable = checkComplementarity(element)
+					index = symbols.index(variable)
+					#print "index", index
+					value[index] = updateValueList(complemented,value,index)
+					#print "val[ind]",value[index]
+					
+	
+	#print "value", value
+	for i in range(len(symbols)):
+		if(value[i] == 1):
+			pureList.append([symbols[i],True])
+		elif(value[i] == 2):
+			pureList.append([symbols[i],False])
+	
+	#print pureList
+	return pureList
+		
+		
+def DPLL(clauses,symbols,var):
+	result = False
+	trueClauses = []
+	#print symbols,"   ", clauses,"     ", var
+	result = True
+	for clause in clauses:
+		#print var, booleanStringClause(clause), eval(booleanStringClause(clause))
+		if(eval(booleanStringClause(clause)) == False):
+			result = False;
+			break;
+		elif(eval(booleanStringClause(clause)) == True):
+			trueClauses.append(clause)
+		else:
+			result = False
+			break;
+			
+	if(result == True):
+		return var
+	
+	else:
+		#print "asdf"
+		#print "end of sentence"
+		pureList = findPureSymbols(clauses,symbols)
+		#print "pure list",pureList
+		trueSymbols = []
+		if(len(pureList)>0):
+			for i in range(len(pureList)):
+				element = pureList[i]
+				index = symbols.index(element[0]);
+				trueSymbols.append(element[0])
+				var[index] = element[1]
+				
+			reqClauses = [x for x in clauses if not(x in trueClauses)]
+			reqSymbols = [x for x in trueSymbols if not(x in symbols)]
+			return DPLL(reqClauses,reqSymbols,var)
+			
+		
+		
 	
 	
 def DPLL_Satisfiable(sentence):
 	symbols = getSymbols(sentence)
 	clauses = getClauses(sentence)
-	#print symbols, clauses
-	return DPLL(clauses,symbols,sentence)
+	var = []
+	for symbol in symbols:
+			var.append('True')
+	return DPLL(clauses,symbols,var), symbols
 
 linenum = 1
 sentCount = -1
@@ -95,6 +184,12 @@ else:
 		clauses = []
 		model = []
 		variable = []
-		index.i=0
-		DPLL_Satisfiable(sentence)
+		print DPLL_Satisfiable(sentence)
 		#print booleanStringSenstence(sentence)
+		
+'''ua = ""
+if(eval('ua or ua')):
+	print "a"
+elif(eval('ua or ua')==False):
+	print "b"
+print eval('not(ua)')'''
